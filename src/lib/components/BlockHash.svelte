14b0e847894a0f93079f1790e834b326a4b24823
Wil Wade <wil.wade@projectliberty.io>
2025-08-14 11:05:43 -0400

    Switch to faster blockies setup

--- src/lib/components/BlockHash.svelte
+++ src/lib/components/BlockHash.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import blockies from 'ethereum-blockies-base64';
+	import { blo, type Address } from 'blo';
 
 	let {
 		hash,
@@ -11,12 +11,12 @@
 		showShortHash?: boolean;
 	} = $props();
 
-	let blockieImage = $derived(blockies(hash));
+	let blockieImage = $derived(blo(hash as Address, size));
 	let shortHash = $derived(hash.slice(0, 10) + '...');
 </script>
 
 <span class="inline-flex items-center gap-1">
-	<img src={blockieImage} alt="Block icon" class="rounded" width={size} height={size} />
+	<img src={blockieImage} alt="" class="rounded" width={size} height={size} />
 	{#if showShortHash}
 		<span class="font-mono text-xs">{shortHash}</span>
 	{:else}
